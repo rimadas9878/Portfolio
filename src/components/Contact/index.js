c63c93394af09224f1bc602d461fc041f8cbe47d
Rima Das <rimadas.automationtester@gmail.com>
2023-01-30 01:00:27 -0800

    updating contact me and home page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,81 +1,91 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { validateEmail } from '../../utils/helpers';
 
+
 export default function ContactMe() {
-    
-    const [formState, setFormState] = useState({
-        name: '',
-        email: '',
-        message: '',
-    });
 
-    const [errorMessage, setErrorMessage] = useState('');
-    const {name, email, message} = formState;
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-    const handleInputChange = (e) => {
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
 
-        if(!errorMessage) {
-            setFormState({...formState, [inputType]: inputValue});
-        }
-    };
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!errorMessage) {
-            console.log('Form submitted', formState);
-            console.log('Handle form', formState);
-        }
-        window.location.href="mailto:dasrima476@gmail.com?subject="+name+"&body="+message
-    };
+    if (!errorMessage) {
+      setFormState({ ...formState, [inputType]: inputValue });
+    }
+  };
 
-return (
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Form submitted', formState);
+      console.log('Handle form', formState);
+    }
+    window.location.href = "mailto:dasrima476@gmail.com?subject=" + name + "&body=" + message
+  };
+
+  return (
+
     <div class="contactPage">
-      <form className="contactForm" onSubmit={handleSubmit}>
-        <div class="nameRow">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            placeholder="Enter your Name"
-            name="name"
-            className="inputName"
-            required
-            onChange={handleInputChange}
-          />
-        </div>
-        <div class="emailRow">
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            placeholder="Enter your Email Address"
-            name="email"
-            className="inputEmail"
-            required
-            onChange={handleInputChange}
-          />
-        </div>
-        <div class="messageRow">
-          <label htmlFor="message" class="message">Message:</label>
-          <textarea
-            type="text"
-            name="message"
-            placeholder="Enter your Message"
-            rows="5"
-            className="inputMessage"
-            required
-            onChange={handleInputChange}
-          />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="errorText">{errorMessage}</p>
+      <div class="connectMsgDiv">
+        <p class="connectMsgText">I'm looking to expand my network with fellow business owners and professionals.<br />
+          I would love to learn about what you do and see if there's any way we can support each other.</p>
+      </div>
+      <div class="contactMeContainer">
+        <form className="contactForm" onSubmit={handleSubmit}>
+          <div class="nameRow">
+            <input
+              type="text"
+              placeholder="Your Name"
+              name="name"
+              className="inputName"
+              required
+              onChange={handleInputChange}
+            />
           </div>
-        )}
-        <button type="submit" class="submit">Submit</button>
-      </form>
+          <div class="emailRow">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              className="inputEmail"
+              required
+              onChange={handleInputChange}
+            />
+          </div>
+          <div class="messageRow">
+            <div class="messageTitleDiv">
+            </div>
+            <textarea
+              type="text"
+              name="message"
+              placeholder="Message"
+              rows="5"
+              className="inputMessage"
+              required
+              onChange={handleInputChange}
+            />
+          </div>
+          {errorMessage && (
+            <div>
+              <p className="errorText">{errorMessage}</p>
+            </div>
+          )}
+          <button type="submit" class="submit">Send</button>
+
+        </form>
+      </div>
+
     </div>
 
-)
+
+  )
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import myPic from '../../assets/me.jpeg';
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaMobileAlt } from "react-icons/fa";
+import { FaMailBulk } from "react-icons/fa";
 
 function Home() {
     return (
-       
-        
+
+
         <div class='homepackBackground'>
             <div class="welcomePage">
                 <h1 class='nameTitle'>RIMA DAS</h1>
@@ -14,6 +17,22 @@ function Home() {
                     class='myImage'
                     src={myPic} alt='mypic'
                 />
+            </div>
+            <div class="personalContactInfo">
+                <div class="personalDetails">
+                    <div class="address">
+                        <FaMapMarkerAlt class="icon" />
+                        <p class="personalDetailsFont">Fremont, CA</p>
+                    </div>
+                    <div class="phoneNo">
+                        <FaMobileAlt class="icon" />
+                        <p class="personalDetailsFont">(510)206-7172</p>
+                    </div>
+                    <div class="emailAddress">
+                        <FaMailBulk class="icon" />
+                        <p class="personalDetailsFont">dasrima476@gmail.com</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
