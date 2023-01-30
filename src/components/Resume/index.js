6e78bdb4adc08a9869c720a320c98fb3b380ac7f
Rima Das <rimadas.automationtester@gmail.com>
2023-01-30 01:28:55 -0800

    Updating the resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,43 +1,63 @@
 import React from "react";
+import Table from 'react-bootstrap/Table';
 
-export default function Resume(){
+export default function Resume() {
     return (
         <div class="resumePage">
-            <ul>
-            <h2>Resume</h2>
-            <hr></hr>
-            <div class="resumePDF">
-            <li><a class="resumetext" href="Rima_Das.pdf">Download Resume</a></li>
+            <div class="resumeContainer">
+                <p class="resumePageFont">Hi,</p>
+                <p class="resumePageFont">This is Rima Das. I hope you're doing well!</p>
+                <p class="resumePageFont">I have attached my resume and cover letter below for your consideration.</p>
+                <div class="resumePDF">
+                    <a class="resumePageFont" href="https://drive.google.com/file/d/1jQlQoPEEFIDgk6mreo5KGmsAZymwqDED/view?usp=share_link">Resume</a> <br/><br/>
+                    <a class="resumePageFont" href="https://drive.google.com/file/d/10A4Z3zwdhlZbe1od9aAajn0Lccsdjx9D/view?usp=share_link">Cover Letter</a>
+                </div>
             </div>
-            <br></br>
-            <br></br>
-            <h2>Front-end Proficiencies</h2>
-            <hr></hr>
-            <div class="frontEndText">
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>jQuery</li>
-            <li>React</li>
-            <li>Bootstrap</li>
+            <div class="proficienciesContainer">
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>Frontend</th>
+                            <th>Backend</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>HTML</td>
+                            <td>API</td>
+                        </tr>
+                        <tr>
+                            <td>CSS</td>
+                            <td>Node</td>
+
+                        </tr>
+                        <tr>
+                            <td>JavaScript</td>
+                            <td>Express</td>
+                        </tr>
+                        <tr>
+                            <td>jQuery</td>
+                            <td>MySQL</td>
+                        </tr>
+                        <tr>
+                            <td>React</td>
+                            <td>Sequelize</td>
+                        </tr>
+                        <tr>
+                            <td>Bootstrap</td>
+                            <td>MongoDB</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Rest</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>GraphQL</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
-            <br></br>
-            <br></br>
-            <h2>Back-end Proficiencies</h2>
-            <hr></hr>
-            <div class="backEndText">
-            <li>APIs</li>
-            <li>Node</li>
-            <li>Express</li>
-            <li>MySQL</li>
-            <li>Sequelize</li>
-            <li>MongoDB, Mongoose</li>
-            <li>Rest</li>
-            <li>GraphQL</li>
-            </div>
-            </ul>
-            
-            
         </div>
     );
 }
